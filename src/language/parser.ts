6db24db27dc8713ce25d6425d3d68167f7f49d04
Fyronix <86478645+Fyronix@users.noreply.github.com>
2023-03-02 19:40:31 +0100

    update parser.ts

--- src/language/parser.ts
+++ src/language/parser.ts
@@ -1,105 +1,102 @@
 import { Instruction } from "./instruction";
 
 export class Parser {
-    private sourceCode: string;
-    private lineNumber: number;
+  private sourceCode: string;
+  private lineNumber = 0;
 
-    constructor(sourceCode: string) {
-        this.sourceCode = sourceCode;
-        this.lineNumber = 0;
+  constructor(sourceCode: string) {
+    this.sourceCode = sourceCode;
+  }
+
+  public parse(): Instruction[] {
+    const instructions: Instruction[] = [];
+    const lines = this.sourceCode.split("\n");
+
+    for (const line of lines) {
+      this.lineNumber++;
+
+      if (line.trim().startsWith("%")) {
+        continue;
+      }
+
+      try {
+        const instruction = this.parseInstruction(line.trim());
+        instructions.push(instruction);
+      } catch (error) {
+        throw new Error(`Error on line ${this.lineNumber}: ${error.message}`);
+      }
     }
 
-    parse(): Instruction[] {
-        const instructions: Instruction[] = [];
+    return instructions;
+  }
 
-        // Split the source code into separate lines
-        const lines = this.sourceCode.split("\n");
+  private parseInstruction(line: string): Instruction {
+    const [operation, argString, addressString] = line.split(" ");
 
-        // Parse each line
-        for (const line of lines) {
-            this.lineNumber++;
-
-            // Skip comments
-            if (line.trim().startsWith("%")) {
-                continue;
-            }
-
-            // Parse the instruction
-            const instruction = this.parseInstruction(line.trim());
-
-            // Add the instruction to the list
-            instructions.push(instruction);
-        }
-
-        return instructions;
+    if (!operation || !argString) {
+      throw new Error("Invalid instruction declaration");
     }
 
-    private parseInstruction(line: string): Instruction {
-        // Extract the operation name and arguments
-        let [operation, argString, addressString] = line.split(" ");
-        if (typeof operation === 'undefined' || typeof argString === 'undefined') {
-            throw Error(`Invalid instruction declaration on line ${this.lineNumber}`);
-        }
-
-        const args = this.parseArguments(argString);
-        const address = this.parseAddress(addressString);
-
-        operation = operation.trim();
-        if (!operation.endsWith(";")) {
-            throw Error(`Invalid syntax, expected ';' after the oepration on line ${this.lineNumber}`);
-        }
-
-        // Create the instruction object
-        const instruction: Instruction = {
-            operation: operation.substring(0, operation.length-1),
-            args,
-            address,
-        };
-
-        return instruction;
+    if (!operation.endsWith(";")) {
+      throw new Error("Expected ';' after the operation");
     }
 
-    private parseArguments(argString: string): (string|number)[] {
-        // Remove the brackets and split the arguments
-        const argList = argString.slice(1, -1).trim().split(",");
+    const args = this.parseArguments(argString);
+    const address = this.parseAddress(addressString);
 
-        // Decode each argument from base64
-        // TODO: if it's an adress, make it a custom type, not string
-        const decodedArgs = argList.map((arg) =>
-            arg.startsWith("@") ? parseInt(arg.substring(1, arg.length)) : atob(arg.trim())
-        );
+    return {
+      operation: operation.slice(0, -1),
+      args,
+      address,
+    };
+  }
 
-        if (decodedArgs.length == 1 && decodedArgs[0] === "") {
-            return []
-        }
-
-        return decodedArgs;
+  private parseArguments(argString: string): (string | number)[] {
+    const trimmedArgString = argString.trim();
+    if (!trimmedArgString.startsWith("[") || !trimmedArgString.endsWith("]")) {
+      throw new Error("Invalid argument format");
     }
 
-    private parseAddress(addressString: string): number | undefined {
-        if (!addressString) {
-            // TODO: optional adress
-            return undefined;
-        }
+    const argList = trimmedArgString
+      .slice(1, -1)
+      .split(",")
+      .map((arg) => arg.trim());
 
-        if (addressString.startsWith("@")) {
-            // Reference to another instruction's address
-            const referencedAddress = parseInt(addressString.slice(1));
-            if (isNaN(referencedAddress)) {
-                throw new Error(
-                    `Error on line ${this.lineNumber}: Invalid address reference`
-                );
-            }
-            return referencedAddress;
-        } else {
-            // Numeric address
-            const address = parseInt(addressString);
-            if (isNaN(address)) {
-                throw new Error(
-                    `Error on line ${this.lineNumber}: Invalid address`
-                );
-            }
-            return address;
-        }
+    if (argList.length === 1 && argList[0] === "") {
+      return [];
     }
+
+    return argList.map((arg) => {
+      if (arg.startsWith("@")) {
+        const address = parseInt(arg.slice(1));
+        if (isNaN(address)) {
+          throw new Error("Invalid address reference");
+        }
+        return address;
+      } else {
+        const decodedArg = atob(arg);
+        return isNaN(+decodedArg) ? decodedArg : +decodedArg;
+      }
+    });
+  }
+
+  private parseAddress(addressString: string): number | undefined {
+    if (!addressString) {
+      return undefined;
+    }
+
+    if (addressString.startsWith("@")) {
+      const referencedAddress = parseInt(addressString.slice(1));
+      if (isNaN(referencedAddress)) {
+        throw new Error("Invalid address reference");
+      }
+      return referencedAddress;
+    } else {
+      const address = parseInt(addressString);
+      if (isNaN(address)) {
+        throw new Error("Invalid address");
+      }
+      return address;
+    }
+  }
 }
